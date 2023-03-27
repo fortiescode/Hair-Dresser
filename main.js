92aa9c89d2d4186443f228beda59e3c910ec5b9f
Hassanain Alarady <106406427+fortiescode@users.noreply.github.com>
2023-03-28 00:25:29 +0200

    javascript function to show mobile nav-bar

---
+++ main.js
@@ -0,0 +1,7 @@
+const mobileSideMenu = document.getElementById("mobile");
+
+const humbMenu = document.querySelector(".humb-menu");
+
+humbMenu.onclick = function (){
+    mobileSideMenu.classList.toggle("show-mobile-menu");
+};
